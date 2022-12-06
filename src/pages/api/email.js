776b95e5fc1da9038c5ed77f39f4c83f2ec869c8
PraedythXIV <praedythxvi@gmail.com>
2022-12-06 01:37:00 +0100

    feat: fixed undefined subject and cleanup

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -1,20 +1,5 @@
 import "dotenv/config"
-//import makeEndpoint from "@/web/services/makeEndPoints"
 import nodemailer from "nodemailer"
-
-// const handler = makeEndpoint({
-//   POST: [
-//     async (req, res) => {
-//       const emailRes = await sendEmail(req.body)
-
-//       if (emailRes.messageId) {
-//         return res.status(200).json({ message: `Email sent successfuly` })
-//       }
-//     },
-//   ],
-// })
-
-// export default handler
 
 const emailSender = async (req, res) => {
   const transporter = nodemailer.createTransport({
@@ -27,20 +12,11 @@ const emailSender = async (req, res) => {
   })
   console.log(req.body)
 
-  // const emailDetails = {
-  //   from: "me@provider.com",
-  //   replyTo: "maybeYou@provider.com",
-  //   to: "you@provider.com",
-  //   subject: "why code no work",
-  //   text: "certified dummy moment",
-  //   html: "This is a certified noob moment as well",
-  // }
-
   const emailDetails = {
     from: `${req.body.email}`,
     replyTo: `${req.body.lastName} <${req.body.email}>`,
     to: "your.email@provider.com",
-    subject: `Message from ${req.body.name}`,
+    subject: `Message from ${req.body.firstName} ${req.body.lastName}`,
     text: `${req.body.message} | Sent from ${req.body.email}`,
     html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`,
   }
